7407be2fd03d37fd896aef569600d5da7427d095
Natasza ASzatan <natiye.nb@gmai.com>
2025-02-27 18:30:20 +0100

    started creating divs containing forms

---
+++ src/App.styled.tsx
@@ -0,0 +1,14 @@
+import { styled } from "@mui/material";
+
+export const StyledDiv = styled("div")`
+  display: flex;
+  flex-direction: row;
+  `
+
+
+export const BoxDiv = styled("div")` 
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+`
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import {BoxDiv, StyledDiv} from "./App.styled.tsx";
+
 function App() {
+  console.log("working")
+
+  const { register, handleSubmit } = useForm();
+  
+  const onSubmit = (data: any) => {
+    console.log("Form Data:", data);
+  };
 
   return (
-    <>
-      
-    </>
+    <StyledDiv>
+      <BoxDiv>
+        <h2>create user</h2>
+        <form onSubmit={handleSubmitRegister(onSubmit)}>
+          <div>
+            <label>Nickname:</label>
+            <input {...register("nickname", { required: true })} />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" {...register("email", { required: true })} />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" {...register("password", { required: true })} />
+          </div>
+          <button type="submit">Create user</button>
+        </form>
+      </BoxDiv>
+      <BoxDiv>
+        <h2>login user</h2>
+      </BoxDiv>
+      <BoxDiv>
+        <h2>post a post</h2>
+      </BoxDiv>
+    </StyledDiv>
   )
 }
 
